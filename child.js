7b0d73c212e6a6beefabbec2bb274c4e69928911
zengxiaoluan <zengxialuan@163.com>
2017-05-05 17:58:13 +0800

    job seeker

--- child.js
+++ child.js
@@ -5,11 +5,11 @@
     /*back to top*/
     $('#top').on('click', function(event) {
         event.preventDefault();
-        $('html,body').animate({scrollTop: 0}, 1000);
+        $('html,body').animate({scrollTop: 0}, 500);
     });
 
     /*求职广告*/
-    // console.log( '%c Give me a better job?\n %c E-mail: %c me@zengxiaoluan.com', 'font-size:30px;color:#1a1a1a;text-align:center;', 'font-size:12px;color:#1a1a1a;','color:red;' );
+    console.log( '%c Give me a better job?\n %c E-mail: %c me@zengxiaoluan.com', 'font-size:30px;color:#1a1a1a;text-align:center;', 'font-size:12px;color:#1a1a1a;','color:red;' );
 
     // color box below
     
