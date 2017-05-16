0bf49817c8c4014968499343e9a5e63f5b2c4b40
zengxiaoluan <zengxialuan@163.com>
2017-05-16 16:47:24 +0800

    change console info

--- child.js
+++ child.js
@@ -9,7 +9,7 @@
     });
 
     /*求职广告*/
-    console.log( '%c Give me a better job?\n %c E-mail: %c me@zengxiaoluan.com', 'font-size:30px;color:#1a1a1a;text-align:center;', 'font-size:12px;color:#1a1a1a;','color:red;' );
+    console.log( '%c Give me a better job?\n %c E-mail: %c me@zengxiaoluan.com', 'background-image:-webkit-gradient( linear, left top,right top, color-stop(0, #00a419),color-stop(0.15, #f44336), color-stop(0.29, #ff4300),color-stop(0.3, #AA00FF),color-stop(0.4, #8BC34A), color-stop(0.45, #607D8B),color-stop(0.6, #4096EE), color-stop(0.75, #D50000),color-stop(0.9, #4096EE), color-stop(1, #FF1A00));color:transparent;-webkit-background-clip:text;font-size:30px;', 'font-size:12px;color:#1a1a1a;','color:red;' );
 
     // color box below
     
