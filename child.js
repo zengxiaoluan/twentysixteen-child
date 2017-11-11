49443c02c037c2f83092d51f5e88c38ed599a749
zeng <zengyuluan@gmail.com>
2017-11-11 23:21:03 +0800

    throttle

--- child.js
+++ child.js
@@ -20,6 +20,25 @@
             }else if ( window.clipboardData ) {
                 return window.clipboardData.setData( 'text', value );
             }
+        },
+        throttle: function(fn, delay, mustRunDelay){
+            var timer = null;
+            var t_start;
+            return function(){
+                var context = this, args = arguments, t_curr = +new Date();
+                clearTimeout(timer);
+                if(!t_start){
+                    t_start = t_curr;
+                }
+                if(t_curr - t_start >= mustRunDelay){
+                    fn.apply(context, args);
+                    t_start = t_curr;
+                } else {
+                    timer = setTimeout(function(){
+                        fn.apply(context, args);
+                    }, delay);
+                }
+            };
         }
     }
 
@@ -79,29 +98,21 @@
                     trickle: false,
                     showSpinner: false,
                     minimum: 0,
-                    trickleSpeed: 100,
+                    trickleSpeed: 0,
                     speed: 100
                 });
             NProgress.start()
 
             var documentHeight = $(document).height();
             var windowHeight = $(window).height();
-            var timer;
 
             scrollHandler();
             function scrollHandler(){
-                clearTimeout(timer);
-                var timer = setTimeout(function(){
-                    var increment = $(window).scrollTop() / (documentHeight - windowHeight);
-                    // console.log(increment)
-                    increment = increment >= 1 ? 0.9999 : increment;
-                    NProgress.set(increment)
-                }, 400);
-
+                var increment = $(window).scrollTop() / (documentHeight - windowHeight);
+                increment = increment >= 1 ? 0.9999 : increment;
+                NProgress.set(increment)
             }
-            $(window).scroll(function(event) {
-                scrollHandler();
-            });
+            $(window).scroll(EventUtil.throttle(scrollHandler, 100));
         }
     }
 
