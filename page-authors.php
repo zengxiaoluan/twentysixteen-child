07acd5ede0060c3d7d685600b749663575b6cdd0
zeng <zengyuluan@gmail.com>
2018-12-02 12:42:39 +0800

    author page sort by user_post_count

--- page-authors.php
+++ page-authors.php
@@ -35,25 +35,51 @@ get_header(); ?>
         <?php
             function contributors() {
                 global $wpdb;
+                $posts = array();
              
                 $authors = $wpdb->get_results("SELECT ID, user_nicename from $wpdb->users ORDER BY ID");
              
                 foreach($authors as $author) {
+                    $one = array();
 
                     $authorID = $author->ID;
 
-                    $display_name = get_the_author_meta('display_name', $authorID);
-
-                    $user_nicename = get_the_author_meta('user_nicename', $authorID);
-
-                    $description = get_the_author_meta('description', $authorID);
-
-                    $user_url = get_the_author_meta('user_url', $authorID);
-
                     $user_post_count = count_user_posts( $authorID );
+                    if (!$user_post_count) continue;
 
+                    $display_name = get_the_author_meta('display_name', $authorID);
+                    $user_nicename = get_the_author_meta('user_nicename', $authorID);
+                    $description = get_the_author_meta('description', $authorID);
+                    $user_url = get_the_author_meta('user_url', $authorID);
                     $link = get_bloginfo('url') . '/author/' . $user_nicename;
+                    
+                    $one['authorID'] = $authorID;
+                    $one['user_post_count'] = $user_post_count;
+                    $one['display_name'] = $display_name;
+                    $one['user_nicename'] = $user_nicename;
+                    $one['description'] = $description;
+                    $one['user_url'] = $user_url;
+                    $one['link'] = $link;
 
+                    $posts[] = $one;
+                }
+
+                function cmp($a, $b){
+                    if ($a['user_post_count'] == $b['user_post_count']) {
+                        return 0;
+                    }
+                    return ($a['user_post_count'] > $b['user_post_count']) ? -1 : 1;
+                }
+                usort($posts, 'cmp');
+
+                foreach($posts as $one) {
+                    $authorID = $one['authorID'];
+                    $user_post_count = $one['user_post_count'];
+                    $display_name = $one['display_name'];
+                    $user_nicename = $one['user_nicename'];
+                    $description = $one['description'];
+                    $user_url = $one['user_url'];
+                    $link = $one['link'];
         ?>
             <li class="one">
                 <a class="avatar-wrap" title="<?php echo $display_name; ?>" target="_blank" href="<?php echo $link; ?>">
@@ -62,7 +88,7 @@ get_header(); ?>
                 <dl class="text">
                     <dt><?php echo $display_name; ?></dt>
                     <dd>
-                        <a target="_blank" href="<?php echo $user_url; ?>">
+                        <a class="user-url" target="_blank" href="<?php echo $user_url; ?>">
                             <?php echo $user_url; ?>
                         </a>
                     </dd>
@@ -115,6 +141,9 @@ get_header(); ?>
     }
     .author-list .avatar:hover {
         border-radius: unset !important;
+    }
+    .user-url {
+        word-break: break-all;
     }
 </style>
 
