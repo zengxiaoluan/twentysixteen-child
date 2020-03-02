21831f9712b35a02481af1966212fdc29d3bb90b
zeng <zengyuluan@gmail.com>
2020-03-02 21:20:31 +0800

    add google adsense

---
+++ includes/google-adsense.php
@@ -0,0 +1,12 @@
+<?php 
+    /**
+     * google adsense
+     * 2020-03-02
+     */
+    function google_adsense(){ ?>
+
+    <script data-ad-client="ca-pub-1203517256266060" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+<?php } 
+
+    add_action( 'wp_head', 'google_adsense' );
