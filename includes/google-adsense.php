557a3f91c9cad12d00fb9c435a2e6ca7777c1727
zeng <zengyuluan@gmail.com>
2020-03-08 13:49:31 +0800

    Only load adsense when user is not logged.

--- includes/google-adsense.php
+++ includes/google-adsense.php
@@ -8,5 +8,6 @@
     <script data-ad-client="ca-pub-1203517256266060" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 
 <?php } 
-
-    add_action( 'wp_head', 'google_adsense' );
+    if (!is_user_logged_in() && !WP_DEBUG) {
+        add_action( 'wp_head', 'google_adsense' );
+    }
