9b7c1b76d5cda83814bcb618e3fc48e7c5372d23
zeng <zengyuluan@gmail.com>
2020-06-14 20:57:09 +0800

    add svg2png

--- child.js
+++ child.js
@@ -6,12 +6,11 @@
  * updated1 2017-11-10
  * updated2 2018-10-25 second day in pingan
  */
-'use strict';
-
-(function ($) {
+'use strict'
+;(function ($) {
   var EventUtil = {
     getClipboardText: function (event) {
-      var clipboardData = (event.clipboardData || window.clipboardData)
+      var clipboardData = event.clipboardData || window.clipboardData
       return clipboardData.getData('text')
     },
     setClipboardText: function (event, value) {
@@ -41,7 +40,7 @@
           }, delay)
         }
       }
-    }
+    },
   }
 
   var modules = {
@@ -49,26 +48,41 @@
       /* back to top */
       $('#top').on('click', function (event) {
         event.preventDefault()
-        $('html,body').animate({scrollTop: 0}, 500)
+        $('html,body').animate({ scrollTop: 0 }, 500)
       })
     },
     jobWanted: function () {
       /* 求职广告 */
-      console.log('%c Give me a better job? %c E-mail: %c me@zengxiaoluan.com', 'background-image:-webkit-gradient( linear, left top,right top, color-stop(0, #00a419),color-stop(0.15, #f44336), color-stop(0.29, #ff4300),color-stop(0.3, #AA00FF),color-stop(0.4, #8BC34A), color-stop(0.45, #607D8B),color-stop(0.6, #4096EE), color-stop(0.75, #D50000),color-stop(0.9, #4096EE), color-stop(1, #FF1A00));color:white;-webkit-background-clip:text;font-size:30px;', 'font-size:12px;color:#1a1a1a;', 'color:red;')
+      console.log(
+        '%c Give me a better job? %c E-mail: %c me@zengxiaoluan.com',
+        'background-image:-webkit-gradient( linear, left top,right top, color-stop(0, #00a419),color-stop(0.15, #f44336), color-stop(0.29, #ff4300),color-stop(0.3, #AA00FF),color-stop(0.4, #8BC34A), color-stop(0.45, #607D8B),color-stop(0.6, #4096EE), color-stop(0.75, #D50000),color-stop(0.9, #4096EE), color-stop(1, #FF1A00));color:white;-webkit-background-clip:text;font-size:30px;',
+        'font-size:12px;color:#1a1a1a;',
+        'color:red;'
+      )
 
-      console.log("%c                            ","background: url(https://zengxiaoluan.com/wp-content/uploads/2016/10/cropped-logo-3.jpg) no-repeat left center;font-size: 128px;");
+      console.log(
+        '%c                            ',
+        'background: url(https://zengxiaoluan.com/wp-content/uploads/2016/10/cropped-logo-3.jpg) no-repeat left center;font-size: 128px;'
+      )
     },
     copyHandler: function () {
-      function addCopyrights (event) {
-        var copyStr = window.getSelection().toString();
-        var limitSize = 20;
-        var enter = '\n\n';
+      function addCopyrights(event) {
+        var copyStr = window.getSelection().toString()
+        var limitSize = 20
+        var enter = '\n\n'
         if (copyStr.length > limitSize) {
-          EventUtil.setClipboardText(event, copyStr + enter + 
-            '标题：' + $('h1').text() + enter +
-            '内容来自曾小乱的blog：' + window.location.href);
-          
-          event.preventDefault();
+          EventUtil.setClipboardText(
+            event,
+            copyStr +
+              enter +
+              '标题：' +
+              $('h1').text() +
+              enter +
+              '内容来自曾小乱的blog：' +
+              window.location.href
+          )
+
+          event.preventDefault()
         }
       }
       document.addEventListener('copy', addCopyrights, false)
@@ -79,14 +93,14 @@
         showSpinner: false,
         minimum: 0,
         trickleSpeed: 1000,
-        speed: 1000
+        speed: 1000,
       })
 
       var documentHeight = $(document).height()
       var windowHeight = $(window).height()
 
       scrollHandler()
-      function scrollHandler () {
+      function scrollHandler() {
         var increment = $(window).scrollTop() / (documentHeight - windowHeight)
         increment = increment >= 1 ? 0.9999 : increment
         window.NProgress.set(increment)
@@ -95,64 +109,78 @@
     },
     contentMenu: function () {
       var h2 = $('.entry-content h2').not('.author-title')
-      
-      if (!$('body.single').length || !h2.length || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) return
+
+      if (
+        !$('body.single').length ||
+        !h2.length ||
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent
+        )
+      )
+        return
 
       // 给 h2 自动加上一个 a 标签
       $.each(h2, function () {
         if (!$(this).find('a').length) {
           var title = $(this).text().replace(/\s+/g, '')
-          $(this).append(`<a id="${title}"></a>`);
+          $(this).append(`<a id="${title}"></a>`)
         }
-      });
+      })
 
       var a = h2.find('a')
       var html = '<ul id="anchors-wrap"><li id="total-menu">文章目录</li>'
 
-      a.each(function(index, el) {
-        html += '<li><a class="anchors" href="#' + el.id + '">' + $(this).parent('h2').text() + '</a></li>'
-      });
+      a.each(function (index, el) {
+        html +=
+          '<li><a class="anchors" href="#' +
+          el.id +
+          '">' +
+          $(this).parent('h2').text() +
+          '</a></li>'
+      })
 
       html += '</ul>'
 
       $('.entry-footer').append(html)
 
-      $('.entry-footer').on('click', '.anchors', function(event) {
-        event.preventDefault()
-        var hash = $(this).attr('href').slice(1)
-        var targetH2 = $('#' + hash).parent('h2')
-        $('html, body').animate({
-          scrollTop: targetH2.offset().top - targetH2.outerHeight(true)
-        });
-        history.replaceState({}, null, '#' + hash)
-      })
-      .end()
-      .on('click', '#total-menu', function (event) {
-        $('html, body').animate({
-          scrollTop: $('h1').offset().top - $('h1').outerHeight(true)
-        });
-        history.replaceState({}, null, '#' + '');
-      });
+      $('.entry-footer')
+        .on('click', '.anchors', function (event) {
+          event.preventDefault()
+          var hash = $(this).attr('href').slice(1)
+          var targetH2 = $('#' + hash).parent('h2')
+          $('html, body').animate({
+            scrollTop: targetH2.offset().top - targetH2.outerHeight(true),
+          })
+          history.replaceState({}, null, '#' + hash)
+        })
+        .end()
+        .on('click', '#total-menu', function (event) {
+          $('html, body').animate({
+            scrollTop: $('h1').offset().top - $('h1').outerHeight(true),
+          })
+          history.replaceState({}, null, '#' + '')
+        })
 
       var top = $('#anchors-wrap').offset().top
 
-      function contentMenuScroller () {
+      function contentMenuScroller() {
         var scrollTop = $(window).scrollTop()
 
         if (top - scrollTop <= 0) {
-          $('#anchors-wrap').addClass('anchors-fixed').css('left', $('.entry-footer').offset().left);
+          $('#anchors-wrap')
+            .addClass('anchors-fixed')
+            .css('left', $('.entry-footer').offset().left)
         } else {
           $('#anchors-wrap').removeClass('anchors-fixed')
         }
-        
+
         if ($('#comments').offset().top - $(window).height() < scrollTop) {
           $('#anchors-wrap').removeClass('anchors-fixed')
         }
       }
 
-      $(window).scroll(EventUtil.throttle(contentMenuScroller, 500, 100));
-
-    }
+      $(window).scroll(EventUtil.throttle(contentMenuScroller, 500, 100))
+    },
   }
 
   // 执行
---
+++ src/svg2png.ts
@@ -0,0 +1,76 @@
+window.addEventListener('load', loadHandler, false)
+
+function loadHandler() {
+  let svgFile = document.querySelector('#svg-file') as HTMLInputElement
+  let main = document.querySelector('#main') as HTMLDivElement
+
+  svgFile.addEventListener('change', function () {
+    let files = this.files as FileList
+    svg2png(files)
+  })
+
+  dropHandler()
+}
+
+function svg2png(files: FileList) {
+  let main = document.querySelector('#main') as HTMLDivElement
+
+  let fReader = new FileReader()
+  loadImageFile(files[0])
+
+  console.log(files)
+
+  fReader.onload = (oFREvent) => {
+    let image = new Image()
+    if (oFREvent.target) {
+      image.src = oFREvent.target.result as string
+
+      image.onload = () => {
+        let canvas = document.createElement('canvas') as HTMLCanvasElement
+        let ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+        console.log(image.width, image.height)
+        canvas.width = image.width
+        canvas.height = image.height
+        ctx.drawImage(image, 0, 0, image.width, image.height)
+
+        // Now is done
+        let png = canvas.toDataURL('image/png')
+
+        let a = document.createElement('a') as HTMLAnchorElement
+        main.appendChild(a)
+        a.href = png
+        a.download = files[0].name + '.png'
+        a.click()
+        main.removeChild(a)
+      }
+    }
+    main.appendChild(image)
+  }
+
+  function loadImageFile(file: any) {
+    fReader.readAsDataURL(file)
+  }
+}
+
+function dropHandler() {
+  let dropRegion = document.querySelector('#drag-svg-file') as HTMLDivElement
+
+  dropRegion.addEventListener('click', function () {
+    let svgFile = document.querySelector('#svg-file') as HTMLInputElement
+    svgFile.click()
+  })
+
+  document.addEventListener('drop', (e) => {
+    e.preventDefault()
+
+    let target = e.target as HTMLElement
+    if (target !== dropRegion) return
+
+    let files = e!.dataTransfer!.files
+    svg2png(files as FileList)
+  })
+}
+
+window.addEventListener('dragover', function (e) {
+  e.preventDefault()
+})
