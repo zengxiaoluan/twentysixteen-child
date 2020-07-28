8a54c2d195b50c6a6f927ea718c9a648a44929d7
zengxiaoluan <me@zengxiaoluan.com>
2020-07-28 16:18:51 +0800

    add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,35 @@
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path');
 
 module.exports = {
+  entry: './src/rss.js',
+  output: {
+    filename: 'rss.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   mode: 'development',
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loader: 'vue-loader'
+        loader: 'vue-loader',
       },
       // 它会应用到普通的 `.js` 文件
       // 以及 `.vue` 文件中的 `<script>` 块
       {
         test: /\.js$/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       // 它会应用到普通的 `.css` 文件
       // 以及 `.vue` 文件中的 `<style>` 块
       {
         test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ]
-      }
-    ]
+        use: ['vue-style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     // 请确保引入这个插件来施展魔法
-    new VueLoaderPlugin()
-  ]
+    new VueLoaderPlugin(),
+  ],
 }
