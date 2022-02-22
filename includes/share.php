007b50c77f61410b1432f04d6643668d3c080074
zeng <zengyuluan@gmail.com>
2022-02-22 12:06:20 +0800

    remove share.js

--- includes/share.php
+++ includes/share.php
@@ -1,6 +1,6 @@
 <?php
 
-add_filter( 'the_content', 'share_the_content' );
+// add_filter( 'the_content', 'share_the_content' );
 function share_the_content( $content ) {
     $page_type = basename( get_page_template() );
     // overpass sildes
@@ -13,7 +13,7 @@ function share_the_content( $content ) {
     return $content;
 }
 
-add_action('wp_enqueue_scripts', 'enqueue_share_styles');
+// add_action('wp_enqueue_scripts', 'enqueue_share_styles');
 function enqueue_share_styles () {
     wp_enqueue_script('share-js', 'https://cdnjs.cloudflare.com/ajax/libs/social-share.js/1.0.16/js/social-share.min.js', array(), '2019-01-01', true);
 
