2e5fd0a68ddfad9034c4479badf5e9c611d49f3a
zeng <zengyuluan@gmail.com>
2023-03-11 18:08:12 +0800

    add epub reader

---
+++ page-epub.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Template Name: epub
+ * 
+ * created at 2023-03-11
+ */
+
+get_header('slides'); ?>
+
+
+
+
+
+    <link href=<?php echo get_stylesheet_directory_uri() . '/dist-epub/static/css/app.3c1b6dec95c0957518ff9b0fd1573928.css'; ?> rel=stylesheet>
+
+
+    <div id=app></div>
+
+    <script type=text/javascript src=<?php echo get_stylesheet_directory_uri() . '/dist-epub/static/js/manifest.3ad1d5771e9b13dbdad2.js'; ?>></script>
+    <script type=text/javascript src=<?php echo get_stylesheet_directory_uri() . '/dist-epub/static/js/vendor.352217ae0725491d320c.js'; ?>></script>
+    <script type=text/javascript src=<?php echo get_stylesheet_directory_uri() . '/dist-epub/static/js/app.d665088e1aedcb9ec192.js'; ?>></script>
+
+
+
+
+<style>
+    .comment-form textarea {
+        background: #f7f7f7;
+        background-image: -webkit-linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0));
+        border: 1px solid #d1d1d1;
+        border-radius: 2px;
+        color: #686868;
+        padding: 0.625em 0.4375em;
+        width: 100%;
+    }
+</style>
+
+<?php get_footer('slides'); ?>
