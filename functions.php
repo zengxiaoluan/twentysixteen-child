70cdb72efd0c04633e898d42b199f22eea93a6be
zengxiaoluan <370203400@qq.com>
2025-04-28 00:19:21 +0800

    add captcha

--- functions.php
+++ functions.php
@@ -9,6 +9,9 @@ require_once get_stylesheet_directory() . '/includes/google-adsense.php';
 // add social share buttons
 require_once get_stylesheet_directory() . '/includes/share.php';
 
+// add captcha
+require_once get_stylesheet_directory() . '/includes/captcha.php';
+
 // See http://core.trac.wordpress.org/ticket/21307
 add_filter( 'pre_option_link_manager_enabled', '__return_true' );
 
@@ -161,36 +164,3 @@ add_filter('handle_bulk_actions-users', function($sendback, $action, $user_ids){
     }
     return admin_url();
 },10,3);
-
-// 添加验证码字段到评论表单
-function add_comment_captcha()
-{
-    $num1 = rand(0, 9);
-    $num2 = rand(0, 9);
-    echo '<p class="comment-form-captcha">
-            <label for="captcha">验证码: ' . $num1 . ' + ' . $num2 . ' = ?<span class="required">*</span></label>
-            <input type="hidden" name="num1" value="' . $num1 . '">
-            <input type="hidden" name="num2" value="' . $num2 . '">
-            <input type="text" name="captcha" id="captcha" size="30">
-          </p>';
-}
-add_action('comment_form_after_fields', 'add_comment_captcha');
-add_action('comment_form_logged_in_after', 'add_comment_captcha');
-
-// 验证验证码
-function verify_comment_captcha($commentdata)
-{
-    if (!isset($_POST['captcha']) || !isset($_POST['num1']) || !isset($_POST['num2'])) {
-        wp_die('错误：请填写验证码。');
-    }
-
-    $sum = (int)$_POST['num1'] + (int)$_POST['num2'];
-    $captcha = (int)$_POST['captcha'];
-
-    if ($captcha !== $sum) {
-        wp_die('错误：验证码不正确，请重试。');
-    }
-
-    return $commentdata;
-}
-add_filter('preprocess_comment', 'verify_comment_captcha');
